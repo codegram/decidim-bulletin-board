65ea74d9b74e9ef24f922667318a888bb9cea42f
Andrea Orler <andrea@codegram.com>
2021-03-10 19:03:21 +0100

    chore: add sidekiq web ui

--- decidim-bulletin_board-app/config/routes.rb
+++ decidim-bulletin_board-app/config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   scope "/api" do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/api"
@@ -31,4 +33,6 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  mount Sidekiq::Web => "/sidekiq"
 end
